ddc99ee2f4a48bd5d49aede074352ae49da41ab0
osiyatech1-admin <osiyatest@gmail.com>
2023-07-11 16:10:42 +0530

    updated design changes

--- src/webparts/heroBanner/components/HeroBanner.tsx
+++ src/webparts/heroBanner/components/HeroBanner.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-// import styles from './HeroBanner.module.scss';
 import { IHeroBannerProps } from './IHeroBannerProps';
-// import { escape } from '@microsoft/sp-lodash-subset';
 import Carousel from 'react-bootstrap/Carousel'
 import '../components/css/heroBanner.css';
 import featureImg from '../components/Images';
@@ -49,13 +47,7 @@ export default class HeroBanner extends React.Component<IHeroBannerProps, IHeroB
                   {
                     this.state.news.length > 0 ? this.getNewsDetailsHTML(this.state.news[0], 'hb_newsCtegory_01') : this.getNewsDetailsHTML(defaultNewsItem, 'hb_newsCtegory_01')
                   }
-                  {/* <a href="#" className='hb_newsCtegory_01' style={{ backgroundImage: `url(${featureImg.feature_img_03})` }}>
-                    <div className='hb_content_wrapper'>
-                      <p className='hb_category_wrapper'>  category 01  </p>
-                      <h3 className='hb_title_wrapper'> need to see a general practitioner our doctors are open for consiltations need to see a general practitioner our doctors are open for consiltations need to see a general practitioner our doctors are open for consiltations need to see a general practitioner our doctors are open for consiltations need to see a general practitioner our doctors are open for consiltations need to see a general practitioner our doctors are open for consiltations    </h3>
-                      <h5 className='hb_date_wrapper'>24-may-2023</h5>
-                    </div>
-                  </a> */}
+
                 </div>
                 <div className='col-md-6'>
                   <div className='row'>
@@ -63,52 +55,26 @@ export default class HeroBanner extends React.Component<IHeroBannerProps, IHeroB
                       {
                         this.state.news.length > 1 ? this.getNewsDetailsHTML(this.state.news[1], 'hb_newsCtegory_02') : this.getNewsDetailsHTML(defaultNewsItem, 'hb_newsCtegory_02')
                       }
-                      {/* <a href="#" className='hb_newsCtegory_02' style={{ backgroundImage: `url(${featureImg.feature_img_04})` }}>
-                        <div className='hb_content_wrapper'>
-                          <p className='hb_category_wrapper'>  category 01  </p>
-                          <h3 className='hb_title_wrapper'> need to see a general practitioner our doctors are open for consiltations </h3>
-                          <h5 className='hb_date_wrapper'>24-may-2023</h5>
-                        </div>
-                      </a> */}
+
                     </div>
                     <div className='col-md-6 column_align_wrapper pdr'>
                       {
-                        this.state.news.length > 2 ? this.getNewsDetailsHTML(this.state.news[1], 'hb_newsCtegory_03') : this.getNewsDetailsHTML(defaultNewsItem, 'hb_newsCtegory_03')
+                        this.state.news.length > 2 ? this.getNewsDetailsHTML(this.state.news[2], 'hb_newsCtegory_03') : this.getNewsDetailsHTML(defaultNewsItem, 'hb_newsCtegory_03')
                       }
-                      {/* <a href="#" className='hb_newsCtegory_03' style={{ backgroundImage: `url(${featureImg.feature_img_05})` }}>
-                        <div className='hb_content_wrapper'>
-                          <p className='hb_category_wrapper'>  category 01  </p>
-                          <h3 className='hb_title_wrapper'> page title </h3>
-                          <h5 className='hb_date_wrapper'>24-may-2023</h5>
-                        </div>
-                      </a> */}
                     </div>
                   </div>
                   <div className='row'>
                     <div className='col-md-6 column_align_wrapper '>
-            
+
                       {
                         this.state.news.length > 3 ? this.getNewsDetailsHTML(this.state.news[3], 'hb_newsCtegory_04') : this.getNewsDetailsHTML(defaultNewsItem, 'hb_newsCtegory_04')
                       }
-                      {/* <a href="#" className='hb_newsCtegory_04' style={{ backgroundImage: `url(${featureImg.feature_img_05})` }}>
-                        <div className='hb_content_wrapper'>
-                          <p className='hb_category_wrapper'>  category 01  </p>
-                          <h3 className='hb_title_wrapper'> page title </h3>
-                          <h5 className='hb_date_wrapper'>24-may-2023</h5>
-                        </div>
-                      </a> */}
+
                     </div>
                     <div className='col-md-6 column_align_wrapper pdr'>
                       {
                         this.state.news.length > 4 ? this.getNewsDetailsHTML(this.state.news[4], 'hb_newsCtegory_05') : this.getNewsDetailsHTML(defaultNewsItem, 'hb_newsCtegory_05')
                       }
-                      {/* <a href="#" className='hb_newsCtegory_05' style={{ backgroundImage: `url(${featureImg.feature_img_04})` }}>
-                        <div className='hb_content_wrapper'>
-                          <p className='hb_category_wrapper'>  category 01  </p>
-                          <h3 className='hb_title_wrapper'> page title </h3>
-                          <h5 className='hb_date_wrapper'>24-may-2023</h5>
-                        </div>
-                      </a> */}
                     </div>
                   </div>
                 </div>
@@ -127,43 +93,6 @@ export default class HeroBanner extends React.Component<IHeroBannerProps, IHeroB
                   {this.getNewsDetailsHTML(item, 'hb_newsCtegory_01')}
                 </Carousel.Item>
               )}
-
-              {/* <Carousel.Item>
-               <a href="#" className='hb_newsCtegory_01' style={{ backgroundImage: `url(${featureImg.feature_img_03})` }}>
-                 <div className='hb_content_wrapper'>
-                   <p className='hb_category_wrapper'>  category 01  </p>
-                   <h3 className='hb_title_wrapper'> need to see a general practitioner our doctors are open for consiltationsneed to see a general practitioner our doctors are open for consiltations </h3>
-                   <h5 className='hb_date_wrapper'>24-may-2023</h5>
-                 </div>
-               </a>
-             </Carousel.Item>
-             <Carousel.Item>
-               <a href="#" className='hb_newsCtegory_01' style={{ backgroundImage: `url(${featureImg.feature_img_04})` }}>
-                 <div className='hb_content_wrapper'>
-                   <p className='hb_category_wrapper'>  category 01  </p>
-                   <h3 className='hb_title_wrapper'> need to see a general practitioner our doctors are open for consiltationsneed. </h3>
-                   <h5 className='hb_date_wrapper'>24-may-2023</h5>
-                 </div>
-               </a>
-             </Carousel.Item>
-             <Carousel.Item>
-               <a href="#" className='hb_newsCtegory_01' style={{ backgroundImage: `url(${featureImg.feature_img_05})` }}>
-                 <div className='hb_content_wrapper'>
-                   <p className='hb_category_wrapper'>  category 01  </p>
-                   <h3 className='hb_title_wrapper'> need to see a general practitioner our doctors are open for consiltationsneed. </h3>
-                   <h5 className='hb_date_wrapper'>24-may-2023</h5>
-                 </div>
-               </a>
-             </Carousel.Item>
-             <Carousel.Item>
-               <a href="#" className='hb_newsCtegory_01' style={{ backgroundImage: `url(${featureImg.feature_img_04})` }}>
-                 <div className='hb_content_wrapper'>
-                   <p className='hb_category_wrapper'>  category 01  </p>
-                   <h3 className='hb_title_wrapper'> need to see a general practitioner our doctors are open for consiltationsneed. </h3>
-                   <h5 className='hb_date_wrapper'>24-may-2023</h5>
-                 </div>
-               </a>
-             </Carousel.Item> */}
             </Carousel>
           </section>
         }
@@ -173,7 +102,6 @@ export default class HeroBanner extends React.Component<IHeroBannerProps, IHeroB
 
   public async componentDidMount() {
     try {
-      //await this.getCategories();
       await this.getAllNews();
     } catch (error) {
       console.log(error)
@@ -214,14 +142,13 @@ export default class HeroBanner extends React.Component<IHeroBannerProps, IHeroB
   public getNewsDetailsHTML(item: News, categoryClassName: string) {
     try {
       return (
-      
+
         <a href={item.pageURL} className={categoryClassName} style={{ backgroundImage: `url(${item.featuredImageURL})` }}>
           <div className='hb_content_wrapper'>
             <p className='hb_category_wrapper'>  {item.category}  </p>
             <h3 className='hb_title_wrapper'>
               {item.title.length > 45 ? item.title.slice(0, 45) + '...' : item.title}
             </h3>
-            {console.log("hgjgjg")}
             <h5 className='hb_date_wrapper'>{item.dateTime != null || item.dateTime != undefined ? moment(item.dateTime).format(newsDateFormat) : ''}</h5>
           </div>
         </a>
